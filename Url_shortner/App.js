d3935c8dbeb9cfaa724b509d9842fca06b04e3dd
chithrakumar <160883882+chithukumar@users.noreply.github.com>
2025-07-05 11:46:44 +0530

    Add files via upload

---
+++ Url_shortner/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React URL Shortner</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
